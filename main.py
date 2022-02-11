586a1a84ca2eb0047dc3875f78834c681c2f2bf5
10-15-5 <hpjpm@protonmail.com>
2022-02-11 17:39:34 +0000

    Methods added

--- main.py
+++ main.py
@@ -3,49 +3,61 @@ import requests
 import json
 
 
-# Creating a connction with the daft.ie website and adding headers
-headers = {"User-Agent": "Mozilla/5.0 (Linux; U; Android 4.2.2; he-il; NEO-X5-116A Build/JDQ39) AppleWebKit/534.30 ("
-                         "KHTML, like Gecko) Version/4.0 Safari/534.30"}
+def main():
 
-response = requests.get("https://www.daft.ie/property-for-rent/kildare?numBeds_from=4", headers=headers)
-webpage = response.content
+    # Creating a connction with the daft.ie website and adding headers
+    headers = {"User-Agent": "Mozilla/5.0 (Linux; U; Android 4.2.2; he-il; NEO-X5-116A Build/JDQ39) AppleWebKit/534.30 ("
+                            "KHTML, like Gecko) Version/4.0 Safari/534.30"}
 
-# Parsing the webpage with Beautiful Soup
-soup = BeautifulSoup(webpage, "html.parser")
+    response = requests.get("https://www.daft.ie/property-for-rent/kildare?numBeds_from=4", headers=headers)
+    webpage = response.content
 
-gaffs = []
+    # Parsing the webpage with Beautiful Soup
+    soup = BeautifulSoup(webpage, "html.parser")
 
-gaff = soup.find_all(class_="TitleBlock__Address-sc-1avkvav-7 hRrWbx")
-beds = soup.find_all(class_="TitleBlock__CardInfoItem-sc-1avkvav-8 uaMgE")
-price = soup.find_all(class_="TitleBlock__StyledSpan-sc-1avkvav-4 bNjvqX")
+    gaffs = parse_webpage(soup)
 
-for i in gaff:
-    gaff = {"address": i.text}
-        
-    gaffs.append(gaff)
+    file_write_and_check(gaffs)
 
-count = 0
-for j in beds:
-    if "Bed" in j.text:
-        gaffs[count].update({"beds": j.text})
+def parse_webpage(soup):
+
+    gaffs = []
+
+    gaff = soup.find_all(class_="TitleBlock__Address-sc-1avkvav-7 hRrWbx")
+    beds = soup.find_all(class_="TitleBlock__CardInfoItem-sc-1avkvav-8 uaMgE")
+    price = soup.find_all(class_="TitleBlock__StyledSpan-sc-1avkvav-4 bNjvqX")
+
+    for i in gaff:
+        gaff = {"address": i.text}
+            
+        gaffs.append(gaff)
+
+    count = 0
+    for j in beds:
+        if "Bed" in j.text:
+            gaffs[count].update({"beds": j.text})
+            count += 1
+
+    count = 0
+    for k in price:
+        k_text = (k.text).replace("€", "")
+        gaffs[count].update({"price": k_text})
         count += 1
 
-count = 0
-for k in price:
-    k_text = (k.text).replace("€", "")
-    gaffs[count].update({"price": k_text})
-    count += 1
 
-#print(gaffs)
+def file_write_and_check(gaffs):
+    # with open("gaffs.json", "r") as file_read:
+    #     json_object = json.load(file_read)
 
-# with open("gaffs.json", "r") as file_read:
-#     json_object = json.load(file_read)
-
-# print(json_object)
-    
-# file_read.close()
+    # print(json_object)
+        
+    # file_read.close()
 
 
-with open("gaffs.json", "a+") as file:
-    file.write(json.dumps(gaffs, indent=4) + ",\n")
-file.close()
+    with open("gaffs.json", "a+") as file:
+        file.write(json.dumps(gaffs, indent=4) + ",\n")
+    file.close()
+
+
+if __name__ == "__main__":
+    main()
